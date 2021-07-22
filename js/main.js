936f8b09895912f4ec889c7ce08ed7af8e952008
AnatolyKostarev <anatolykostarev@gmail.com>
2021-07-22 14:14:46 +0300

    Add function for animation burger menu by menu items

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@
 let menuBtn = document.querySelector('.headerNavigation_button');
 let menu = document.querySelector('.headerNavigation_hidden');
 let fixedBody = document.querySelector('.scrollOn');
+let mobileMenuBtn = document. querySelectorAll('.js-link');
 
 let hideMenu = () => {
     menuBtn.classList.toggle('headerNavigation_button_active');
     menu.classList.toggle('active');
     fixedBody.classList.toggle('scrollOff');
 }
-
 menuBtn.addEventListener('click', hideMenu);
+
+mobileMenuBtn.forEach(item => (
+    item.addEventListener('click', hideMenu)
+));
