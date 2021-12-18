38c3573b54b8c0ab9c2cf9cc710d9a869bcfa6a4
Anatoly <anatolykostarev@gmail.com>
2021-12-18 19:08:13 +0300

    Fix

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
-'use strict'
+"use strict";
 
-let menuBtn = document.querySelector('.headerNavigation_button');
-let menu = document.querySelector('.headerNavigation_hidden');
-let fixedBody = document.querySelector('.scrollOn');
-let mobileMenuBtn = document. querySelectorAll('.js-link');
+let menuBtn = document.querySelector(".headerNavigation_button");
+let menu = document.querySelector(".headerNavigation_hidden");
+// let fixedBody = document.querySelector(".scrollOn");
+let mobileMenuBtn = document.querySelectorAll(".js-link");
 
-let hideMenu = () => {
-    menuBtn.classList.toggle('headerNavigation_button_active');
-    menu.classList.toggle('active');
-    fixedBody.classList.toggle('scrollOff');
-}
-menuBtn.addEventListener('click', hideMenu);
+const $titleLink = [...document.querySelectorAll(".js-scrollLink")];
 
-mobileMenuBtn.forEach(item => (
-    item.addEventListener('click', hideMenu)
-));
+const hideMenu = () => {
+  menuBtn.classList.toggle("headerNavigation_button_active");
+  menu.classList.toggle("active");
+  document.body.classList.toggle("scrollOff");
+};
+menuBtn.addEventListener("click", hideMenu);
+
+mobileMenuBtn.forEach((item) => item.addEventListener("click", hideMenu));
+
+const handleTitle = (event) => {
+  event.preventDefault();
+  const href = event.target.getAttribute("href");
+  const elem = document.querySelector(href);
+
+  document.documentElement.scrollTo({
+    top: elem.offsetTop,
+    behavior: "smooth",
+  });
+};
+
+$titleLink.forEach((elem) => elem.addEventListener("click", handleTitle));
